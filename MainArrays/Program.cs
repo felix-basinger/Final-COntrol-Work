4ae7bd57052d4262a10062bc8e4abbdd9d3161dd
Basinger <fel_simon@mail.ru>
2023-04-28 19:29:49 +0300

    Added file MainArrays with first changes

---
+++ MainArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задача: Написать программу, которая из имеющегося массива строк формирует 
+// новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать 
+// на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
+// коллекциями, лучше обойтись исключительно массивами.
+
+int Length(string arg)
+{
+    int num;
+    System.Console.Write($"Enter: {arg}: ");
+    while(!int.TryParse(Console.ReadLine(), out num) || num < 1)
+    {
+        System.Console.WriteLine("It's not a number or incorrect data!");
+        System.Console.Write($"Enter {arg}: ");
+    }
+    return num;
+}
+
+string[] CreateArray()
+{
+    System.Console.WriteLine("Hello!");
+    int count = Length("How many elements do you want to see in your array? Your answer: ");
+    string[] array = new string[count];
+    return array;
+}
+
+ 
